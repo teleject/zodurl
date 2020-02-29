57506bc1082a7c8d01427598a5c9c4dc0e3f1ce0
teleject <schmitt@christopher.org>
2020-02-29 14:36:11 -0600

    Adding comments ... fer learnin'

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 module.exports = function(config) {
+    
+    // allow certain files to be moved to distribution folder
     config.addPassthroughCopy('favicon.ico');
     config.addPassthroughCopy('.htaccess');
   
+    // change files outputs for GitHub pages' folder
     return {
         dir: { output: 'docs' }
     };
