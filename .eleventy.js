56c896091c58be30c2621b3cfdaf781ef88e3968
teleject <schmitt@christopher.org>
2020-03-01 10:02:46 -0600

    Adding CNAME to built gh-pages

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(config) {
     // allow certain files to be moved to distribution folder
     config.addPassthroughCopy('favicon.ico');
     config.addPassthroughCopy('.htaccess');
+    config.addPassthroughCopy('CNAME');
   
     // change files outputs for GitHub pages' folder
     return {
