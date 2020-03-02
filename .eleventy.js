7c797c0417d2729782982b088f92343668cba77a
teleject <schmitt@christopher.org>
2020-03-01 18:00:10 -0600

    Add og social image

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(config) {
     
     // allow certain files to be moved to distribution folder
+    config.addPassthroughCopy('zodurl-og-image.jpg');
     config.addPassthroughCopy('favicon.ico');
     config.addPassthroughCopy('.htaccess');
     config.addPassthroughCopy('CNAME');
